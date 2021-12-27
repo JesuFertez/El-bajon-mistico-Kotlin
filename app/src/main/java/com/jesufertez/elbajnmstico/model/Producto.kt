5cec0042530452e7a252fa52ea87c8c0cff5e1dd
Jesufertez <maria.fernandez.cortez@gmail.com>
2021-12-26 22:15:06 -0300

    Initial commit

---
+++ app/src/main/java/com/jesufertez/elbajnmstico/model/Producto.kt
@@ -0,0 +1,9 @@
+package com.jesufertez.elbajnmstico.model
+
+data class Producto(
+    val id: Int,
+    val name:String,
+    val description:String,
+    val urlImage:String,
+    val price: Int
+)
