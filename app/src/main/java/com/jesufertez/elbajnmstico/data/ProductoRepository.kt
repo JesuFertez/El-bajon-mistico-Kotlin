a7e214e1b10e3763d37ddf21acea64fb4eb403f6
Jesufertez <maria.fernandez.cortez@gmail.com>
2021-12-27 01:10:31 -0300

    Se agrega capa de presentation

--- app/src/main/java/com/jesufertez/elbajnmstico/data/ProductoRepository.kt
+++ app/src/main/java/com/jesufertez/elbajnmstico/data/ProductoRepository.kt
@@ -2,7 +2,8 @@ package com.jesufertez.elbajnmstico.data
 
 import com.jesufertez.elbajnmstico.data.remote.ProductosApi
 
-class ProductoRepository (private val productoApi : ProductosApi){
-
+class ProductoRepository (
+    private val productoApi : ProductosApi)
+{
     suspend fun getProducto()= productoApi.obtenerProducto()
 }
